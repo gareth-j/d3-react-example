7b24b9de523a3ea8fb740f20d92db8e620f5f8f8
Gareth Jones <oxidising@gmail.com>
2020-10-15 17:49:53 +0100

    Updates

---
+++ precache-manifest.62698b18349137e8a7dbad10dc2bb625.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2620801a1070eee4de99f5ae70f2394",
+    "url": "/d3-react-example/index.html"
+  },
+  {
+    "revision": "79e513be2bd10f26099c",
+    "url": "/d3-react-example/static/css/2.391527b9.chunk.css"
+  },
+  {
+    "revision": "ebf5abab4c682766ec14",
+    "url": "/d3-react-example/static/css/main.a15078f8.chunk.css"
+  },
+  {
+    "revision": "79e513be2bd10f26099c",
+    "url": "/d3-react-example/static/js/2.7953c8b7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/d3-react-example/static/js/2.7953c8b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebf5abab4c682766ec14",
+    "url": "/d3-react-example/static/js/main.541a2fe0.chunk.js"
+  },
+  {
+    "revision": "03faed29451b8e557bb9",
+    "url": "/d3-react-example/static/js/runtime-main.342b46f9.js"
+  },
+  {
+    "revision": "2f12242375edd68e9013ecfb59c672e9",
+    "url": "/d3-react-example/static/media/fa-brands-400.2f122423.svg"
+  },
+  {
+    "revision": "8300bd7f30e0a313c1d772b49d96cb8e",
+    "url": "/d3-react-example/static/media/fa-brands-400.8300bd7f.ttf"
+  },
+  {
+    "revision": "ad527cc5ec23d6da66e8a1d6772ea6d3",
+    "url": "/d3-react-example/static/media/fa-brands-400.ad527cc5.woff"
+  },
+  {
+    "revision": "e2ca6541bff3a3e9f4799ee327b28c58",
+    "url": "/d3-react-example/static/media/fa-brands-400.e2ca6541.eot"
+  },
+  {
+    "revision": "f075c50f89795e4cdb4d45b51f1a6800",
+    "url": "/d3-react-example/static/media/fa-brands-400.f075c50f.woff2"
+  },
+  {
+    "revision": "3602b7e8b2cb1462b0bef9738757ef8a",
+    "url": "/d3-react-example/static/media/fa-regular-400.3602b7e8.svg"
+  },
+  {
+    "revision": "3c6879c4f342203d099bdd66dce6d396",
+    "url": "/d3-react-example/static/media/fa-regular-400.3c6879c4.woff"
+  },
+  {
+    "revision": "49f00693b0e5d45097832ef5ea1bc541",
+    "url": "/d3-react-example/static/media/fa-regular-400.49f00693.ttf"
+  },
+  {
+    "revision": "4a74738e7728e93c4394b8604081da62",
+    "url": "/d3-react-example/static/media/fa-regular-400.4a74738e.woff2"
+  },
+  {
+    "revision": "b01516c1808be557667befec76cd6318",
+    "url": "/d3-react-example/static/media/fa-regular-400.b01516c1.eot"
+  },
+  {
+    "revision": "205f07b3883c484f27f40d21a92950d4",
+    "url": "/d3-react-example/static/media/fa-solid-900.205f07b3.ttf"
+  },
+  {
+    "revision": "4451e1d86df7491dd874f2c41eee1053",
+    "url": "/d3-react-example/static/media/fa-solid-900.4451e1d8.woff"
+  },
+  {
+    "revision": "664de3932dd6291b4b8a8c0ddbcb4c61",
+    "url": "/d3-react-example/static/media/fa-solid-900.664de393.svg"
+  },
+  {
+    "revision": "8ac3167427b1d5d2967646bd8f7a0587",
+    "url": "/d3-react-example/static/media/fa-solid-900.8ac31674.eot"
+  },
+  {
+    "revision": "8e1ed89b6ccb8ce41faf5cb672677105",
+    "url": "/d3-react-example/static/media/fa-solid-900.8e1ed89b.woff2"
+  }
+]);
