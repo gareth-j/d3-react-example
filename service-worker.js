b6ee75afe1c70d8fa24cc23fac9b2e49f8b231e5
gareth-j <8915182+gareth-j@users.noreply.github.com>
2020-10-16 12:51:06 +0000

    Deploying to gh-pages from  @ 07304570ae784d1756e4d1b11b0ac6521205f518 🚀

--- service-worker.js
+++ service-worker.js
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/d3-react-example/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
