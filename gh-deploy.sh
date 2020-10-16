3aae0a6bf730e87d561d57286c6b74ac3bf7a23a
Gareth Jones <oxidising@gmail.com>
2020-10-16 10:00:44 +0100

    Adding automated gh-pages deployment

---
+++ gh-deploy.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -eu
+
+repo_uri="https://x-access-token:${DEPLOY_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+remote_name="origin"
+main_branch="main"
+target_branch="gh-pages"
+build_dir="build"
+
+cd "$GITHUB_WORKSPACE"
+
+git config user.name "$GITHUB_ACTOR"
+git config user.email "${GITHUB_ACTOR}@bots.github.com"
+
+git checkout "$target_branch"
+git rebase "${remote_name}/${main_branch}"
+
+npm run build
+git add "$build_dir"
+
+git commit -m "Auto update GitHub Pages"
+if [ $? -ne 0 ]; then
+    echo "Nothing to commit"
+    exit 0
+fi
+
+git remote set-url "$remote_name" "$repo_uri" # includes access token
+git push --force-with-lease "$remote_name" "$target_branch"
